1a56170d1f257741c59226912ccc315299805504
Stanley <85762462+stanley709ml@users.noreply.github.com>
2026-01-23 12:17:48 -0300

    Added "square_check.c"

---
+++ square_check.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+int isPerfectSquare(int number);
+
+int main(){
+
+	int x = 0;
+	int xSqrt = 0;
+
+	printf("Choose a number to check if it's a perfect square: ");
+	scanf("%d", &x);
+
+	if (isPerfectSquare(x)){
+		xSqrt = sqrt(x);
+		printf("%d is a perfect square from %d\n", x, xSqrt);
+	} else{
+		printf("%d is not a perfect square.\n", x);
+	}
+
+	return 0;
+}
+
+int isPerfectSquare(int number){
+	int i;
+	
+	for(int i = 0; i * i = number; i++){
+		if (i * i == number){
+			return 1;
+		} else{
+			return 0;		
+		}
+	}
+}
