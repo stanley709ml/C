c7c25220223ea47dd4558dd72105dd7519bdacc2
Stanley <85762462+stanley709ml@users.noreply.github.com>
2026-01-24 23:46:12 -0300

    Updated "freight_pricing.c"

--- freight_pricing.c
+++ freight_pricing.c
@@ -1,17 +1,25 @@
-#include <stdio.h>
+#include<stdio.h>
+
+void freight_price(float km);
 
 int main(){
+    
+    float km = 0.0f;
+    
+    printf("How many kilometers away is your home from our company? ");
+    scanf("%f", &km);
+    
+    freight_price(km);
+    
+    return 0;
+}
 
-	float km_away = 0.0f;
-	const float dolperkm = 0.39;
-	float total_price = 0.0f;
+void freight_price(float km){
 
-	printf("How many kilometers away is your home from our company? ");
-	scanf("%f", &km_away);
-
-	total_price = km_away * dolperkm;
-
-	printf("Your total freight price is $%.2f ($%.2f per km)\n", total_price, dolperkm);
-
-	return 0;
+    const float dolPerKm = 0.39;
+    float total = 0.0f;
+    
+    total = km * dolPerKm;
+    
+    printf("The total freight cost is $%.2f ($%.2f per km)\n", total, dolPerKm);
 }
