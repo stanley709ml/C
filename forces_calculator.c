017374160c1ed59747e22227449bf255c760632a
Stanley <stanley.martins@proton.me>
2026-04-11 22:38:36 -0300

    Improved forces_calculator.c by making the data of each force into structs.

--- forces_calculator.c
+++ forces_calculator.c
@@ -8,6 +8,16 @@ typedef double f64;
 
 // Weight Force
 
+typedef struct {
+	char get_weightForce[20];
+	char get_mass[20];
+	char get_gravity[20];
+	int choice_weightForce;
+	f64 weightForce;
+	f64 mass;
+	f64 gravity;
+} Weight_Force;
+
 f64 calculate_weightForce(f64 mass, f64 gravity);
 f64 calculate_massFrom_weightForce(f64 force, f64 gravity);
 f64 calculate_gravityFrom_weightForce(f64 force, f64 mass);
@@ -18,6 +28,16 @@ f64 calculate_gravityFrom_weightForce(f64 force, f64 mass);
 
 // Friction Force
 
+typedef struct {
+	char get_frictionForce[20];
+	char get_frictionConstant[20];
+	char get_normalForce[20];
+	int choice_frictionForce;
+	f64 frictionForce;
+	f64 frictionConstant;
+	f64 normalForce;
+} Friction_Force;
+
 f64 calculate_frictionForce(f64 friction_coeficient, f64 normal_force);
 f64 calculate_frictionCoeficient_from_frictionForce(f64 frictionForce, f64 normal_force);
 f64 calculate_normalForce_from_frictionForce(f64 frictionForce, f64 friction_coeficient);
@@ -27,6 +47,16 @@ f64 calculate_normalForce_from_frictionForce(f64 frictionForce, f64 friction_coe
 // ---------------- // 
 
 // Elastic Force
+
+typedef struct {
+	char get_springForce[20];
+	char get_springConstant[20];
+	char get_springDisplacement[20];
+	int choice_springForce;
+	f64 springForce;
+	f64 springConstant;
+	f64 springDisplacement;
+} Elastic_Force;
 
 f64 calculate_springForce(f64 spring_constant, f64 spring_displacement);
 f64 calculate_springConstant_from_springForce(f64 springForce, f64 spring_displacement);
@@ -57,176 +87,159 @@ int main() {
 
 		switch (choice) {
 			case 1:
-				char get_weightForce[20];
-				char get_mass[20];
-				char get_gravity[20];
-				int choice_weightForce = 0;
-				f64 weightForce = 0;
-				f64 mass = 0;
-				f64 gravity = 0;
-				printf("Firsts firsts, tell me what do you already have:\n1 - I have both mass and gravity, but no force yet\n2 - I have both force and gravity, but no mass\n3 - I have both force and mass, but no gravity.\nAnswer: ");
-				choice_weightForce = read_int_choice();
+				Weight_Force weight_force;
 
-				switch (choice_weightForce) {
+				printf("Firsts firsts, tell me what do you already have:\n1 - I have both mass and gravity, but no force yet\n2 - I have both force and gravity, but no mass\n3 - I have both force and mass, but no gravity.\nAnswer: ");
+				weight_force.choice_weightForce = read_int_choice();
+
+				switch (weight_force.choice_weightForce) {
 					case 1:
 						printf("Tell me the mass (kg): ");
-						fgets(get_mass, sizeof(get_mass), stdin);
-						sscanf(get_mass, "%lf", &mass);
+						fgets(weight_force.get_mass, sizeof(weight_force.get_mass), stdin);
+						sscanf(weight_force.get_mass, "%lf", &weight_force.mass);
 				
 						printf("Tell me the gravity (m/s^2): ");
-						fgets(get_gravity, sizeof(get_gravity), stdin);
-						sscanf(get_gravity, "%lf", &gravity);
+						fgets(weight_force.get_gravity, sizeof(weight_force.get_gravity), stdin);
+						sscanf(weight_force.get_gravity, "%lf", &weight_force.gravity);
 
-						weightForce = calculate_weightForce(mass, gravity);
+						weight_force.weightForce = calculate_weightForce(weight_force.mass, weight_force.gravity);
 
-						printf("Weight Force = %.2lfN\n", weightForce);
+						printf("Weight Force = %.2lfN\n", weight_force.weightForce);
 						break;
 					case 2:
 						printf("Tell me the force (N): ");
-						fgets(get_weightForce, sizeof(get_weightForce), stdin);
-						sscanf(get_weightForce, "%lf", &weightForce);
+						fgets(weight_force.get_weightForce, sizeof(weight_force.get_weightForce), stdin);
+						sscanf(weight_force.get_weightForce, "%lf", &weight_force.weightForce);
 
 						printf("Tell me the gravity (m/s^2): ");
-						fgets(get_gravity, sizeof(get_gravity), stdin);
-						sscanf(get_gravity, "%lf", &gravity);
+						fgets(weight_force.get_gravity, sizeof(weight_force.get_gravity), stdin);
+						sscanf(weight_force.get_gravity, "%lf", &weight_force.gravity);
 
-						gravity = calculate_massFrom_weightForce(weightForce, gravity);
+						weight_force.gravity = calculate_massFrom_weightForce(weight_force.weightForce, weight_force.gravity);
 
-						printf("Mass = %.2lfkg\n", gravity);
+						printf("Mass = %.2lfkg\n", weight_force.gravity);
 						break;
 					case 3:
 						printf("Tell me the force (N): ");
-						fgets(get_weightForce, sizeof(get_weightForce), stdin);
-						sscanf(get_weightForce, "%lf", &weightForce);
+						fgets(weight_force.get_weightForce, sizeof(weight_force.get_weightForce), stdin);
+						sscanf(weight_force.get_weightForce, "%lf", &weight_force.weightForce);
 
 						printf("Tell me the mass (kg): ");
-						fgets(get_mass, sizeof(get_mass), stdin);
-						sscanf(get_mass, "%lf", &mass);
+						fgets(weight_force.get_mass, sizeof(weight_force.get_mass), stdin);
+						sscanf(weight_force.get_mass, "%lf", &weight_force.mass);
 
-						gravity = calculate_gravityFrom_weightForce(weightForce, mass);
+						weight_force.gravity = calculate_gravityFrom_weightForce(weight_force.weightForce, weight_force.mass);
 
-						printf("Gravity = %.2lfm/s^2\n", gravity);
+						printf("Gravity = %.2lfm/s^2\n", weight_force.gravity);
 						break;
 					}
-				strcpy(get_weightForce, "");
-				strcpy(get_mass, "");
-				strcpy(get_gravity, "");
+				strcpy(weight_force.get_weightForce, "");
+				strcpy(weight_force.get_mass, "");
+				strcpy(weight_force.get_gravity, "");
 				break;
 		
 			case 2:
-				char get_frictionForce[20];
-				char get_frictionConstant[20];
-				char get_normalForce[20];
-				int choice_frictionForce = 0;
-				f64 frictionForce = 0;
-				f64 frictionConstant = 0;
-				f64 normalForce = 0;
+				Friction_Force friction_force;
 
 				printf("Firsts firsts, tell me what do you already have:\n1 - I have both friction constant and normal force, but no friction force yet\n2 - I have both friction force and friction constant, but no normal force.\n3 - I have both friction force and normal force, but no friction constant.\nAnswer: ");
-				choice_frictionForce = read_int_choice();
+				friction_force.choice_frictionForce = read_int_choice();
 
-				switch (choice_frictionForce) {
+				switch (friction_force.choice_frictionForce) {
 					case 1:
 						printf("Tell me the friction constant (N/m): ");
-						fgets(get_frictionConstant, sizeof(get_frictionConstant), stdin);
-						sscanf(get_frictionConstant, "%lf", &frictionConstant);
+						fgets(friction_force.get_frictionConstant, sizeof(friction_force.get_frictionConstant), stdin);
+						sscanf(friction_force.get_frictionConstant, "%lf", &friction_force.frictionConstant);
 
 						printf("Tell me the normal force (N): ");
-						fgets(get_normalForce, sizeof(get_normalForce), stdin);
-						sscanf(get_normalForce, "%lf", &normalForce);
+						fgets(friction_force.get_normalForce, sizeof(friction_force.get_normalForce), stdin);
+						sscanf(friction_force.get_normalForce, "%lf", &friction_force.normalForce);
 
-						frictionForce = calculate_frictionForce(frictionConstant, normalForce);
+						friction_force.frictionForce = calculate_frictionForce(friction_force.frictionConstant, friction_force.normalForce);
 
-						printf("Friction Force = %.2lfN\n", frictionForce);
+						printf("Friction Force = %.2lfN\n", friction_force.frictionForce);
 						break;
 					case 2:
 						printf("Tell me the friction force (N): ");
-						fgets(get_frictionForce, sizeof(get_frictionForce), stdin);
-						sscanf(get_frictionForce, "%lf", &frictionForce);
+						fgets(friction_force.get_frictionForce, sizeof(friction_force.get_frictionForce), stdin);
+						sscanf(friction_force.get_frictionForce, "%lf", &friction_force.frictionForce);
 
 						printf("Tell me the friction constant (N/m): ");
-						fgets(get_frictionConstant, sizeof(get_frictionConstant), stdin);
-						sscanf(get_frictionConstant, "%lf", &frictionConstant);
+						fgets(friction_force.get_frictionConstant, sizeof(friction_force.get_frictionConstant), stdin);
+						sscanf(friction_force.get_frictionConstant, "%lf", &friction_force.frictionConstant);
 
-						normalForce = calculate_normalForce_from_frictionForce(frictionForce, frictionConstant);
+						friction_force.normalForce = calculate_normalForce_from_frictionForce(friction_force.frictionForce, friction_force.frictionConstant);
 
-						printf("Normal Force = %.2lfN\n", normalForce);
+						printf("Normal Force = %.2lfN\n", friction_force.normalForce);
 						break;
 					case 3:
 						printf("Tell me the friction force (N): ");
-						fgets(get_frictionForce, sizeof(get_frictionForce), stdin);
-						sscanf(get_frictionForce, "%lf", &frictionForce);
+						fgets(friction_force.get_frictionForce, sizeof(friction_force.get_frictionForce), stdin);
+						sscanf(friction_force.get_frictionForce, "%lf", &friction_force.frictionForce);
 
 						printf("Tell me the normal force (N): ");
-						fgets(get_normalForce, sizeof(get_normalForce), stdin);
-						sscanf(get_normalForce, "%lf", &normalForce);
+						fgets(friction_force.get_normalForce, sizeof(friction_force.get_normalForce), stdin);
+						sscanf(friction_force.get_normalForce, "%lf", &friction_force.normalForce);
 
-						frictionConstant = calculate_frictionCoeficient_from_frictionForce(frictionForce, normalForce);
+						friction_force.frictionConstant = calculate_frictionCoeficient_from_frictionForce(friction_force.frictionForce, friction_force.normalForce);
 
-						printf("Friction Constant = %.2lfn/m\n", frictionConstant);
+						printf("Friction Constant = %.2lfn/m\n", friction_force.frictionConstant);
 						break;
 					}
-				strcpy(get_frictionForce, "");
-				strcpy(get_frictionConstant, "");
-				strcpy(get_normalForce, "");
+				strcpy(friction_force.get_frictionForce, "");
+				strcpy(friction_force.get_frictionConstant, "");
+				strcpy(friction_force.get_normalForce, "");
 				break;
 
 			case 3:
-				char get_springForce[20];
-				char get_springConstant[20];
-				char get_springDisplacement[20];
-				int choice_springForce = 0;
-				f64 springForce = 0;
-				f64 springConstant = 0;
-				f64 springDisplacement = 0;
+				Elastic_Force elastic_force;
 				
 				printf("Firsts firsts, tell me what do you already have:\n1 - I have both spring constant and spring displacement, but no spring force yet\n2 - I have both spring force and spring constant, but no spring displacement.\n3 - I have both spring force and spring displacement, but no spring constant.\nAnswer: ");
-				choice_springForce = read_int_choice();
+				elastic_force.choice_springForce = read_int_choice();
 
-				switch (choice_springForce) {
+				switch (elastic_force.choice_springForce) {
 					case 1:
 						printf("Tell me the spring constant (N/m):");
-						fgets(get_springConstant, sizeof(get_springConstant), stdin);
-						sscanf(get_springConstant, "%lf", &springConstant);
+						fgets(elastic_force.get_springConstant, sizeof(elastic_force.get_springConstant), stdin);
+						sscanf(elastic_force.get_springConstant, "%lf", &elastic_force.springConstant);
 
 						printf("Tell me the spring displacement (m): ");
-						fgets(get_springDisplacement, sizeof(get_springDisplacement), stdin);
-						sscanf(get_springDisplacement, "%lf", &springDisplacement);
+						fgets(elastic_force.get_springDisplacement, sizeof(elastic_force.get_springDisplacement), stdin);
+						sscanf(elastic_force.get_springDisplacement, "%lf", &elastic_force.springDisplacement);
 
-						springForce = calculate_springForce(springConstant, springDisplacement);
+						elastic_force.springForce = calculate_springForce(elastic_force.springConstant, elastic_force.springDisplacement);
 
-						printf("Spring Force = %.2lfN\n", springForce);
+						printf("Spring Force = %.2lfN\n", elastic_force.springForce);
 						break;
 					case 2:
 						printf("Tell me the spring force (N): ");
-						fgets(get_springForce, sizeof(get_springForce), stdin);
-						sscanf(get_springForce, "%lf", &springForce);
+						fgets(elastic_force.get_springForce, sizeof(elastic_force.get_springForce), stdin);
+						sscanf(elastic_force.get_springForce, "%lf", &elastic_force.springForce);
 
 						printf("Tell me the spring constant (N/m): ");
-						fgets(get_springConstant, sizeof(get_springConstant), stdin);
-						sscanf(get_springConstant, "%lf", &springConstant);
+						fgets(elastic_force.get_springConstant, sizeof(elastic_force.get_springConstant), stdin);
+						sscanf(elastic_force.get_springConstant, "%lf", &elastic_force.springConstant);
 
-						springDisplacement = calculate_springDisplacement_from_springForce(springForce, springConstant);
+						elastic_force.springDisplacement = calculate_springDisplacement_from_springForce(elastic_force.springForce, elastic_force.springConstant);
 
-						printf("Spring Displacement = %.2lfm\n", springDisplacement);
+						printf("Spring Displacement = %.2lfm\n", elastic_force.springDisplacement);
 						break;
 					case 3:
 						printf("Tell me the spring force (N): ");
-						fgets(get_springForce, sizeof(get_springForce), stdin);
-						sscanf(get_springForce, "%lf", &springForce);
+						fgets(elastic_force.get_springForce, sizeof(elastic_force.get_springForce), stdin);
+						sscanf(elastic_force.get_springForce, "%lf", &elastic_force.springForce);
 
 						printf("Tell me the spring displacement (m): ");
-						fgets(get_springDisplacement, sizeof(get_springDisplacement), stdin);
-						sscanf(get_springDisplacement, "%lf", &springDisplacement);
+						fgets(elastic_force.get_springDisplacement, sizeof(elastic_force.get_springDisplacement), stdin);
+						sscanf(elastic_force.get_springDisplacement, "%lf", &elastic_force.springDisplacement);
 
-						springConstant = calculate_springConstant_from_springForce(springForce, springDisplacement);
+						elastic_force.springConstant = calculate_springConstant_from_springForce(elastic_force.springForce, elastic_force.springDisplacement);
 
-						printf("Spring Constant = %.2lfN/m\n", springConstant);
+						printf("Spring Constant = %.2lfN/m\n", elastic_force.springConstant);
 						break;
 				}
-				strcpy(get_springForce, "");
-				strcpy(get_springConstant, "");
-				strcpy(get_springDisplacement, "");
+				strcpy(elastic_force.get_springForce, "");
+				strcpy(elastic_force.get_springConstant, "");
+				strcpy(elastic_force.get_springDisplacement, "");
 				break;
 		} 
 
